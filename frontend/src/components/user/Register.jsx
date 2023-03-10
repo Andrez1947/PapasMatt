5bb13d668a40a6213184933253626d3e3932f7f8
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-03-10 08:23:48 -0500

    fixing categories component into menu page

--- frontend/src/components/user/Register.jsx
+++ frontend/src/components/user/Register.jsx
@@ -48,50 +48,107 @@ const Register = ({ history }) => {
     dispatch(register(formData));
   };
 
+  const onChange = e => {
+    if(e.target.name === 'avatar') {
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+
+      }
+
+      reader.readAsDataURL(e.target.files[0])
+
+    } else {
+      
+    }
+  }
+
   return (
-    <div class="container mx-auto p-4 bg-white">
-      <div class="w-full md:w-1/2 lg:w-1/3 mx-auto my-12">
-        <h1 class="text-lg font-bold">Register</h1>
-        <form class="flex flex-col mt-4">
-          <input
-            type="text"
-            name="full-name"
-            class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-            placeholder="Full Name"
-          />
-          <input
-            type="email"
-            name="email"
-            class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-            placeholder="Email address"
-          />
-          <input
-            type="password"
-            name="password"
-            class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-            placeholder="Password"
-          />
-          <input
-            type="password"
-            name="repeat-password"
-            class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-            placeholder="Repeat Password"
-          />
-          <button
-            type="submit"
-            class="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white focus:outline-none bg-blue-500  hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex items-center w-full justify-center font-medium"
+    <Fragment>
+      <div className="container mx-auto p-4 bg-white">
+        <div className="w-full md:w-1/2 lg:w-1/3 mx-auto my-12">
+          <h1 className="text-lg font-bold">Register</h1>
+          <form
+            className="flex flex-col mt-4"
+            onSubmit={handleSubmit}
+            encType="multipart/form-data"
           >
-            Register
-          </button>
-          <div class="flex flex-col items-center mt-5">
-            <p class="mt-1 text-xs font-light text-gray-500">
-              Register already?
-              <a class="ml-1 font-medium text-blue-400">Sign in now</a>
-            </p>
-          </div>
-        </form>
+            <label htmlFor="form-group">Nombre</label>
+            <input
+              type="name"
+              id="name_field"
+              className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm form-control"
+              name="name"
+              value={name}
+              onChange={onChange}
+              placeholder="¿Cómo quieres que te llamemos?"
+            />
+            <label htmlFor="form-group">Correo Electrónico</label>
+            <input
+              type="email"
+              id="email_field"
+              className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Correo electrónico"
+            />
+            <label htmlFor="form-group">Contraseña</label>
+            <input
+              type="password"
+              id="password_field"
+              className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Elige una contraseña"
+            />
+            <div className="form-group">
+              <label for="avatar_upload">Avatar</label>
+              <div className="d-flex align-items-center">
+                <div>
+                  <figure className="avatar mr-3 item-rtl">
+                    <img 
+                    src={avatarPreview} 
+                    className="rounded-circle" 
+                    alt="Avatar Preview"                    
+                    />
+                  </figure>
+                </div>
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    name="avatar"
+                    className="custom-file-input"
+                    id="customFile"
+                    accept="images/*"
+                    onChange={onChange}
+                  />
+                  <label className="custom-file-label" for="customFile">
+                    Choose Avatar
+                  </label>
+                </div>
+              </div>
+            </div>
+            <button
+              id="register_button"
+              type="submit"
+              className="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white focus:outline-none bg-blue-500  hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex items-center w-full justify-center font-medium"
+              disabled={ loading ? true : false }
+            >
+              Registrarse
+            </button>
+            <div className="flex flex-col items-center mt-5">
+              <p className="mt-1 text-xs font-light text-gray-500">
+                ¿Ya estás registrado?
+                <a href="/" className="ml-1 font-medium text-blue-400">Ingresa Ahora</a>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
