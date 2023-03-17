d2d40889719a5050bf9e10bc6e1984605e4d0520
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-03-16 20:52:13 -0500

    adding cloudinary

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const bodyparser = require('body-parser');
+const cloudinary = require('cloudinary');
+
+
 const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 //Import all routes
