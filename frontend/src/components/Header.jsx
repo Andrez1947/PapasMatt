b15aec5587c443e3dfcebddd29aaff3ec1345f5f
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-04-07 16:59:46 -0500

    Logout component done and tested

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
   const [isMenu, SetIsMenu] = useState(false);
 
   const logoutHandler = () => {
-    dispatchEvent(logout());
+    dispatch(logout());
     alert.success("Cierre de sesión exitoso");
   };
 
@@ -94,8 +94,9 @@ const Header = () => {
                           Mis Pedidos
                         </p>
                       </Link>
-                      <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
-                        Salir <MdLogout />
+                      <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base "
+                      onClick={logoutHandler}>
+                        Salir <MdLogout />                        
                       </p>
                     </div>
                   )}
@@ -176,7 +177,8 @@ const Header = () => {
                         Mis Pedidos
                       </p>
                     </Link>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                    onClick={logoutHandler}>
                       Salir <MdLogout />
                     </p>
                   </div>
