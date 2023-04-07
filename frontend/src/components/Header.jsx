ebe89a64640d60aa1bb2ea2870ec7483127d9fbe
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-04-07 16:43:51 -0500

    mobile header working succesful

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -136,13 +136,51 @@ const Header = () => {
           <div className="relative">
             {user ? (
               <div className="ml-0 dropdown d-inline">
-                <Link to="#!">
+                <Link to="#!" onClick={() => SetIsMenu(!isMenu)}>
                   <div class="space-y-2 mr-5">
                     <div class="w-8 h-0.5 bg-gray-600"></div>
                     <div class="w-8 h-0.5 bg-gray-600"></div>
                     <div class="w-8 h-0.5 bg-gray-600"></div>
                   </div>
                 </Link>
+                {isMenu && (
+                  <div className="absolute top-full right-0 mt-0 w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col px-0 py-2">
+                    {user && user.rol === "admin" && (
+                      <Link to="/product/new">
+                        <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                          New Item <MdAdd />
+                        </p>
+                      </Link>
+                    )}
+                    <ul                      
+                      className="col-1 gap-8"
+                    >
+                      <li className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                        Home
+                      </li>
+                      <li className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                        Menú
+                      </li>                      
+                      <li className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                        Servicio
+                      </li>
+                    </ul>
+
+                    <Link to="/me">
+                      <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                        Mi Perfil
+                      </p>
+                    </Link>
+                    <Link to="/orders/me">
+                      <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                        Mis Pedidos
+                      </p>
+                    </Link>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
+                      Salir <MdLogout />
+                    </p>
+                  </div>
+                )}
               </div>
             ) : (
               !loading && (
