a76838bdf5234a452902d533cd849a3de7be7175
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-04-09 14:40:56 -0500

    UpdateProfile donde and working

--- frontend/src/components/user/UpdateProfile.jsx
+++ frontend/src/components/user/UpdateProfile.jsx
@@ -56,49 +56,52 @@ const UpdateProfile = () => {
 
   return (
     <Fragment>
-      <div class="max-w-screen-lg mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8 text-center">
+      <div className="max-w-screen-lg mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8 text-center">
         <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-full before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto mb-10 text-center">
           Mi Perfil
         </p>
-        <div class="mt-8 bg-white shadow-lg rounded-lg">
-          <div class="p-6">
-            <form action="#" method="POST">
-              <div class="mb-8">
+        <div className="mt-8 bg-white shadow-lg rounded-lg">
+          <div className="p-6">
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <div className="mb-8">
                 <label
-                  for="name"
-                  class="block text-lg font-medium text-gray-900 mb-2"
+                  htmlFor="name"
+                  className="block text-lg font-medium text-gray-900 mb-2"
                 >
                   Nombre
                 </label>
                 <input
                   type="text"
-                  id="name"
+                  id="name_field"
                   name="name"
-                  class="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                  className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500 form-control"
                   placeholder="Ingresa tu nombre"
-                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
-              <div class="mb-8">
+              <div className="mb-8">
                 <label
-                  for="email"
-                  class="block text-lg font-medium text-gray-900 mb-2"
+                  htmlFor="email"
+                  className="block text-lg font-medium text-gray-900 mb-2"
                 >
                   Correo Electrónico
                 </label>
                 <input
                   type="email"
-                  id="email"
+                  id="email_field"
                   name="email"
-                  class="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                  className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   placeholder="Ingresa tu correo electrónico"
-                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div class="flex justify-end">
+              <div className="flex justify-end">
                 <button
                   type="submit"
-                  class="bg-indigo-500 text-white px-4 py-2 rounded-md text-sm focus:outline-none focus:shadow-outline-gray"
+                  className="bg-indigo-500 text-white px-4 py-2 rounded-md text-sm focus:outline-none focus:shadow-outline-gray"
+                  disabled={loading ? true : false}
                 >
                   Actualizar
                 </button>
