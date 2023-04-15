624383f11a5cebe437471c5f3325b71c06d6cd05
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-04-14 21:25:23 -0500

    cart done

--- frontend/src/components/product/CartContainer.jsx
+++ frontend/src/components/product/CartContainer.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
+import { RiRefreshFill } from "react-icons/ri";
+import { BiMinus, BiPlus } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 const CartContainer = () => {
   return (
-    <div>CartContainer</div>
-  )
-}
+    <div className="fixed top-0  w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col z-[101]">
+      <div className="w-full flex items-center justify-between p-4 cursor-pointer">
+        <motion.div whileTap={{ scale: 0.75 }}>
+          {" "}
+          <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
+        </motion.div>
+        <p className="text-textColor text-lg font-semibold">Tu Carrito</p>
+        <motion.p
+          whileTap={{ scale: 0.75 }}
+          className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover: shadow-md cursor-pointer text-textColor text-base "
+        >
+          Limpiar <RiRefreshFill />
+        </motion.p>
+      </div>
+      {/*Bottom section*/}
+      <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
+        <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-scroll scroll-none">
+          {/*Cart Item*/}
+          <div className="w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2">
+            <img
+              src="https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Product"
+              className="w-25 h-25 max-w-[60px] rounded-full object-contain"
+            />
+            {/*name section*/}
+            <div className="flex flex-col gap-2">
+              <p className="text-base text-gray-50"> Montañeras</p>
+              <p className="text-sm block text-gray-300 font-semibold">
+                $18.000
+              </p>
+            </div>
+            {/*button section*/}
+            <div className="group flex items-center gap-2 ml-auto cursor-pointer">
+              <motion.div whileTap={{ scale: 0.75 }} className="">
+                <BiMinus className="text-gray-50 " />
+              </motion.div>
+              <p className="w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center">
+                1
+              </p>
+              <motion.div whileTap={{ scale: 0.75 }} className="">
+                <BiPlus className="text-gray-50 " />
+              </motion.div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CartContainer
+export default CartContainer;
