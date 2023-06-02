3b4c97d475da4009044ad67b810e932b3b1fb322
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-06-02 16:41:18 -0500

    ok

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -7,46 +7,48 @@ mercadopago.configure({
 });
 
 // Procesar pagos con Mercado Pago => /api/v1/payment/process
-exports.processPayment = catchAsyncErrors(async (req, res, next, successUrl, failureUrl, pendingUrl) => {
-  const { amount } = req.body;
+exports.processPayment = catchAsyncErrors(
+  async (req, res, next, successUrl, failureUrl, pendingUrl) => {
+    const { amount } = req.body;
 
-  const preference = {
-    back_urls: {
-      failure:'http://localhost:3000/api/v1/payment/success',
-      pending:'http://localhost:3000/api/v1/payment/success',
-      success: 'http://localhost:3000/api/v1/payment/success'
-    },
-    items: [
-      {
-        title: "Pedido Papas Matt",        
-        unit_price: parseInt(amount),
-        quantity: 1,        
+    const preference = {
+      back_urls: {
+        failure: "https://http://localhost:3000/api/v1/payment/failure",
+        pending: "http://localhost:3000/api/v1/payment/pending",
+        success: "https://http://localhost:3000/api/v1/payment/success",
       },
-    ], 
-    notification_url: `http://localhost:3000/api/v1/payment/success/${123}`,       
-    payment_methods: {
-      excluded_payment_types: [
-        // Excluir métodos de pago que no deseas mostrar       
+      items: [
+        {
+          title: "Pedido Papas Matt",
+          unit_price: parseInt(amount),
+          quantity: 1,
+        },
       ],
-      installments: 12, // Configurar el número máximo de cuotas permitidas
-    },
-    payment_type_ids: ["credit_card", "ticket", "bank_transfer"], // Incluir opciones de pago adicionales
-    external_reference: "ID_DE_REFERENCIA_EXTERNA", // Puedes proporcionar una referencia externa personalizada para rastrear la transacción    
-  };
+      notification_url: `http://localhost:3000/api/v1/notificar`,
+      payment_methods: {
+        excluded_payment_types: [
+          // Excluir métodos de pago que no deseas mostrar
+        ],
+        installments: 12, // Configurar el número máximo de cuotas permitidas
+      },
+      payment_type_ids: ["credit_card", "ticket", "bank_transfer"], // Incluir opciones de pago adicionales
+      external_reference: "ID_DE_REFERENCIA_EXTERNA", // Puedes proporcionar una referencia externa personalizada para rastrear la transacción
+    };
 
-  const response = await mercadopago.preferences.create(preference);
-  const { id, init_point } = response.body;
+    const response = await mercadopago.preferences.create(preference);
+    const { id, init_point } = response.body;
 
-  res.status(200).json({
-    success: true,
-    payment_id: id,
-    payment_url: init_point,
-    success_url: successUrl,
-    failure_url: failureUrl,
-    pending_url: pendingUrl,
-  });
-    console.log(response)
-});
+    res.status(200).json({
+      success: true,
+      payment_id: id,
+      payment_url: init_point,
+      success_url: successUrl,
+      failure_url: failureUrl,
+      pending_url: pendingUrl,
+    });
+    console.log(response);
+  }
+);
 
 // Send mercadopago API Key => /api/v1/mercadopagoapi
 exports.sendMercadopagoApi = catchAsyncErrors(async (req, res, next) => {
--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,24 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { 
-    processPayment,
-    sendMercadopagoApi
- } = require('../controllers/paymentController');
+const {
+  processPayment,
+  sendMercadopagoApi,
+} = require("../controllers/paymentController");
 
-const {isAuthenticatedUser} = require('../middlewares/auth');
+const { isAuthenticatedUser } = require("../middlewares/auth");
 
 // Create payment => /api/v1/order/new
-router.route('/payment/process').post(isAuthenticatedUser, (req, res, next) => {
-    // Agrega las backurls a la respuesta
-    const successUrl = '/payment/success';
-    const failureUrl = "'/payment/failure'";
-    const pendingUrl = "'/payment/pending'";
+router.route("/payment/process").post(isAuthenticatedUser, (req, res, next) => {
+  // Agrega las backurls a la respuesta
+  const successUrl = "/payment/success";
+  const failureUrl = "'/payment/failure'";
+  const pendingUrl = "'/payment/pending'";
 
-    processPayment(req, res, next, successUrl, failureUrl, pendingUrl);
+  processPayment(req, res, next, successUrl, failureUrl, pendingUrl);
 });
 
 // Send api by frontend
-router.route('/mercadopagoapi').get(isAuthenticatedUser, sendMercadopagoApi);
+router.route("/mercadopagoapi").get(isAuthenticatedUser, sendMercadopagoApi);
+
+router
+  .route("/notificar")
+  .post(isAuthenticatedUser, (req, res, next) => {
+    console.log('notificar') 
+    const {body, query} = req;
+    console.log({body, query})
+    res.sendStatus(200);    
+  });
+
+  router
+  .route("/notificar")
+  .get( (req, res, next) => {
+    console.log('notificar') 
+    const {body, query} = req;
+    console.log({body, query})
+    res.sendStatus(200);    
+  });
 
 module.exports = router;
